e6beda56ab8de57b5b4651ea4eaf0296dff667bc
Abhijeet Rohilla <nishuinhurry@gmail.com>
2025-01-09 08:36:04 +0530

    Node app day 2

--- src/app.js
+++ src/app.js
@@ -2,18 +2,22 @@ const express =require('express');
 
 const app = express();
 
-app.use('/test',(req,res)=>{
+app.get('/test',(req,res)=>{
     res.send("Hello Test")
 })
 
 
-app.use('/about',(req,res)=>{
+app.post('/test:id',(req,res)=>{
+    console.log(req.params);    
     res.send("Hello About")
 })
 
-app.use('/',(req,res)=>{
-    res.send("Hello Home")
+app.delete('/test',(req,res)=>{
+    console.log(req);
+    res.send("Deleted Succesfully")
+    
 })
+
 
 app.listen(3000,()=>{
     console.log("Server Running ....");
